f44935b025e6accfc9fa8d9b2030c109dc104f21
Trent Shailer <trent.shailer@gmail.com>
2021-03-01 19:32:34 +1300

    Commented SpriteSaturator

--- Color Blind 2.0/Assets/Scripts/Tools/SpriteSaturator.cs
+++ Color Blind 2.0/Assets/Scripts/Tools/SpriteSaturator.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class SpriteSaturator : MonoBehaviour
 {
+	// Mostly self explanatory, this script saturated the attached spriteRenderer
+	// Based on the saturation value in GameManager
+	// The spriteRenderer must have the SaturationShader Material
     public SpriteRenderer sr;
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
-      //  sr.material.SetFloat("_Saturation", HUDManager.singleton.saturation);
     }
 
 
