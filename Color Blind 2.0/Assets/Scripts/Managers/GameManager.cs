a40f101c6d63eef22242c06bf08987b19aed6f20
Trent Shailer <trent.shailer@gmail.com>
2021-03-01 16:58:43 +1300

    New basic project setup and ready for porting

---
+++ Color Blind 2.0/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager singleton;
+	void Awake(){
+		singleton = this;
+	}
+
+	// All game manager related things go in here
+	// E.g. Player stats
+	// Which are all read by other scripts
+
+}
