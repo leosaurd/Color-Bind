88ba41d9c0e94ee5f0bbc33e531c0b893981824c
Trent Shailer <trent.shailer@gmail.com>
2021-03-05 16:53:00 +1300

    Further test enemy setup and health bar

---
+++ Color Blind 2.0/Assets/Scripts/Game/Enemies/EnemyHealthbar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthbar : MonoBehaviour
+{
+	private GameObject healthBar;
+	private GameObject background;
+	private EnemyBase controller;
+	Vector3 initialScale;
+
+	private void Awake()
+	{
+		controller = transform.parent.GetComponent<EnemyBase>();
+		healthBar = transform.GetChild(0).gameObject;
+		background = transform.GetChild(1).gameObject;
+		initialScale = healthBar.transform.localScale;
+
+	}
+
+	private void Update()
+	{
+		float percent = controller.stats.health / controller.stats.maxHealth;
+		healthBar.transform.localScale = new Vector3(initialScale.x * percent, initialScale.y, initialScale.z);
+	}
+}
